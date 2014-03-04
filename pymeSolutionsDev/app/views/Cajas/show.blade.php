c81c9b4119d7354ad79997d8d4ae561f57a35ecd
Luis Robles Coulter <roblescoulter@gmail.com>
2014-03-03 18:32:16 -0600

    show view

--- pymeSolutionsDev/app/views/Cajas/show.blade.php
+++ pymeSolutionsDev/app/views/Cajas/show.blade.php
@@ -2,37 +2,40 @@
 
 @section('main')
 
-<h1>Show Caja</h1>
+<div class="page-header clearfix">
+      <h3 class="pull-left">Caja &gt; <small>{{{ $Caja->VEN_Caja_Codigo }}}</small></h3>
+      <div class="pull-right">
+        <a href="{{{ URL::to('Cajas') }}}" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Back</a>
+      </div>
+</div>
 
-<p>{{ link_to_route('Cajas.index', 'Return to all Cajas') }}</p>
+<div class="form-group">
+        <h4>ID# : {{{ $Caja->VEN_Caja_id }}}</h4>
+</div>
+<div class="form-group">
+        <h4>Código de Caja : {{{ $Caja->VEN_Caja_Codigo }}}</h4>
+</div>
+<div class="form-group">
+        <h4>Número de Caja : {{{ $Caja->VEN_Caja_Numero }}}</h4>
+</div>
+<div class="form-group"><h4>Estado : 
+	@if($Caja->VEN_Caja_Estado == 1)
+        Activada
+    @else
+    	Desactivada
+    @endif
+    </h4>    
+</div>
+<div class="form-group">
+        <h4>Saldo Inicial : {{{ $Caja->VEN_Caja_SaldoInicial }}}</h4>
+</div>
 
-
-<table class="table table-striped table-bordered">
-	<thead>
-		<tr>
-			<th>VEN_Caja_id</th>
-				<th>VEN_Caja_Codigo</th>
-				<th>VEN_Caja_Numero</th>
-				<th>VEN_Caja_Estado</th>
-				<th>VEN_Caja_SaldoInicial</th>
-		</tr>
-	</thead>
-
-	<tbody>
-		<tr>
-			<td>{{{ $Caja->VEN_Caja_id }}}</td>
-					<td>{{{ $Caja->VEN_Caja_Codigo }}}</td>
-					<td>{{{ $Caja->VEN_Caja_Numero }}}</td>
-					<td>{{{ $Caja->VEN_Caja_Estado }}}</td>
-					<td>{{{ $Caja->VEN_Caja_SaldoInicial }}}</td>
-                    <td>{{ link_to_route('Cajas.edit', 'Edit', array($Caja->VEN_Caja_id), array('class' => 'btn btn-info')) }}</td>
-                    <td>
-                        {{ Form::open(array('method' => 'DELETE', 'route' => array('Cajas.destroy', $Caja->VEN_Caja_id))) }}
-                            {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
-                        {{ Form::close() }}
-                    </td>
-		</tr>
-	</tbody>
-</table>
+<div class="form-group">
+		{{ link_to_route('Cajas.edit', 'Edit', array($Caja->VEN_Caja_id), array('class' => 'btn btn-info')) }}
+	    {{ Form::open(array('method' => 'DELETE', 'route' => array('Cajas.destroy', $Caja->VEN_Caja_id))) }}
+	        	{{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+	    	{{ Form::close() }}
+</div>
+                     
 
 @stop
