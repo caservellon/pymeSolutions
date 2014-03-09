619e8aec600b8eaa60548276ba789b8a974bb2e5
jairovelasquez <jairod_velalvarado@hotmail.com>
2014-03-09 03:44:30 +0000

    Translations

---
+++ pymeSolutionsDev/app/lang/es/reminders.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "La contraseña debe tener al menos seis caracteres y conincidir con la validación.",
+    "password" => "Passwords must be at least six characters and match the confirmation.",
+
+    "user" => "No se encuentra ningún usuario con esa dirección de correo.",
+    "user" => "We can't find a user with that e-mail address.",
+
+    "token" => "Este token para reiniciar la contraseña es inválido.",
+    "token" => "This password reset token is invalid.",
+
+    "sent" => "¡Recordatorio de contraseña enviado!",
+    "sent" => "Password reminder sent!",
+
+);
