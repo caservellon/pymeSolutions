e8697d11ea5a7281a792feddcdfbdccd67a67d2b
Carlos Canales <canalesav@unitec.edu>
2014-03-10 23:31:45 -0600

    configuracion de estado de orden

--- pymeSolutionsDev/app/routes.php
+++ pymeSolutionsDev/app/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function(){
 	return View::make('hello');
-<<<<<<< HEAD
+
 });    
 //crea un nuevo estado de orden de compras
 Route::get('/Compras/Configuracion/EstadoOrden/Nuevo',array('as'=>'NuevoEstadoOrdenCompra','uses'=>'COMEstadoOrdenCompraController@NuevoEstadoOrden'));
@@ -38,8 +38,8 @@ Route::get('/Compras/Configuracion/TransicionEstado/Lista', array('as'=>'ListaTr
 
 Route::resource('COMEstadoOrdenCompras', 'COMEstadoOrdenCompraController');
 Route::resource('COMTransicionEstado', 'COMTransicionEstadoController');
-=======
-});
+
+
 Route::group(array('prefix' => 'Compras'), function(){
     
     Route::get('Cotizacions/parametrizar', array('as'=>'parametrizar', 'uses'=> 'CotizacionsController@parametrizar'));
@@ -53,13 +53,12 @@ Route::group(array('prefix' => 'Compras'), function(){
     Route::get('Cotizacions/actualizar', array('as'=>'actualizar', 'uses'=>'CotizacionsController@actualizar'));
     Route::resource('Cotizacions', 'CotizacionsController');
 });
-//Route::get('Cotizacions/parametrizar', array('as'=>'parametrizar', 'uses'=> 'CotizacionsController@parametrizar'));
-//Route::get('Cotizacions/mensaje', array('as'=>'mensaje', 'uses'=> 'CotizacionsController@mensaje'));
-////Route::get('parametrizar', 'CotizacionsController@parametrizar');
-//Route::post('Cotizacions/campoLocal', array('as'=>'campoLocal', 'uses'=> 'CotizacionsController@campoLocal'));
+Route::get('Cotizacions/parametrizar', array('as'=>'parametrizar', 'uses'=> 'CotizacionsController@parametrizar'));
+Route::get('Cotizacions/mensaje', array('as'=>'mensaje', 'uses'=> 'CotizacionsController@mensaje'));
+Route::get('parametrizar', 'CotizacionsController@parametrizar');
+Route::post('Cotizacions/campoLocal', array('as'=>'campoLocal', 'uses'=> 'CotizacionsController@campoLocal'));
 ////index de editar parametrizar
-//Route::get('Cotizacions/editarParametrizar', array('as'=>'editarParametrizar', 'uses'=>'CotizacionsController@editarParametrizar'));
-//Route::get('Cotizacions/actualizar', array('as'=>'actualizar', 'uses'=>'CotizacionsController@actualizar'));
-//Route::resource('Cotizacions', 'CotizacionsController');
+Route::get('Cotizacions/editarParametrizar', array('as'=>'editarParametrizar', 'uses'=>'CotizacionsController@editarParametrizar'));
+Route::get('Cotizacions/actualizar', array('as'=>'actualizar', 'uses'=>'CotizacionsController@actualizar'));
+Route::resource('Cotizacions', 'CotizacionsController');
 
->>>>>>> compras
