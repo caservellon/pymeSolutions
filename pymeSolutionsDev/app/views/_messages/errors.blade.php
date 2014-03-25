5eee9e84d1b96c2000446071b4fe76ab60f1ebfd
andrestorrez <andrews_torres@hotmail.com>
2014-03-25 16:52:39 -0600

    View errors imporved

--- pymeSolutionsDev/app/views/_messages/errors.blade.php
+++ pymeSolutionsDev/app/views/_messages/errors.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="<?php public_path(); ?>/bootstrap/docs.min.css">
 @if ($errors->any())
-<div class="bs-callout bs-callout-danger error">
+<div class="alert alert-danger error">
     <ul >
         {{ implode('', $errors->all('<li class="error">:message</li>')) }}
     </ul>
