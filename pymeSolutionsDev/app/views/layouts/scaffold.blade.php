db84758ca11d9158dcb09af9d3eac204fba1c46a
andrestorrez <andrews_torres@hotmail.com>
2014-04-28 18:13:10 -0600

    navbar modificada para que sea responsive

--- pymeSolutionsDev/app/views/layouts/scaffold.blade.php
+++ pymeSolutionsDev/app/views/layouts/scaffold.blade.php
@@ -12,123 +12,138 @@
 
 </head>
 <body style="margin-top: 50px">
-  <header>
-  <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-    <a class="navbar-brand" href="#">pyme Solutions</a>
-
-    <ul class="nav navbar-nav navbar-left">
-      <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ventas <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="/Ventas/Ventas/create">Gestión de Ventas</a></li>
-              <li><a href="/Ventas/Cajas">Gestión de Caja</a></li>
-              <li><a href="/Ventas/Devoluciones/create">Devoluciones</a></li>
-              <li class="divider"></li>
-              <li><a href="/Ventas">Configuración</a></li>
-            </ul>
-          </li>
-
-      <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Compras <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="/Compras/SolicitudCotizacion">Solicitudes de Cotización</a></li>
-              <li><a href="#">Cotizaciones</a></li>
-              <li><a href="#">**Ordenes de Compras</a></li>
-              <li><a href="/Compras/OrdenCompra/sinCotizacion/ListaProductos" >Crear Orden Compra sin Cotizacion</a></li>
-              <li><a href="/Compras/OrdenCompra/conCotizacion/ListaCotizaciones" >Comparar Cotizaciones</a></li>
-              <li><a href="/Compras/OrdenCompra/Autorizacion/ListaOrdenes" > Autorizar Orden Compra</a></li>
-              <li><a href="/Compras/OrdenCompra/Autorizacion/ListarOrdenes" > Administrar Orden Compra</a></li>
-              <li><a href="/Compras/OrdenCompra/GenerarPago/ListaCotizaciones" > generar pago Ordenes de  Compras</a></li>
-              <li><a href="/Compras/OrdenCompra/Historial/ListarOrdenes" > Historial Ordenes de  Compras</a></li>
-              <li class="divider"></li>
-              <li><a href="/Compras">Configuración</a></li>
-            </ul>
-          </li>
-
-      <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Inventario <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li>
-                {{ link_to_route('Inventario.Productos.index', 'Productos')}}
+  
+  <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">pyme Solutions</a>
+      </div>
+        
+        <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ventas <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/Ventas/Ventas/create">Gestión de Ventas</a></li>
+                  <li><a href="/Ventas/Cajas">Gestión de Caja</a></li>
+                  <li><a href="/Ventas/Devoluciones/create">Devoluciones</a></li>
+                  <li class="divider"></li>
+                  <li><a href="/Ventas">Configuración</a></li>
+                </ul>
               </li>
-              <li>
-                {{ link_to_route('Inventario.Atributos.index', 'Atributos')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.Proveedor.index', 'Proveedores')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.Categoria.index', 'Categorías')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.Ciudad.index', 'Ciudades')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.UnidadMedidas.index', 'Unidades de Medida')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.Horarios.index', 'Horarios')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.FormaPagos.index', 'Formas de Pagos')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.MovimientoInventario.index', 'Movimiento Inventario')}}
-              </li>
-              <li>
-                {{ link_to_route('Inventario.MotivoMovimiento.index', 'Motivo Movimiento Inventario')}}
-              </li>
-              <li class="divider"></li>
-              <li>
-                 <a href="/Inventario">Configuración</a>
-              </li>
-            </ul>
-          </li>
 
           <li class="dropdown">
-
-            <a href="contabilidad" class="dropdown-toggle" data-toggle="dropdown">Contabilidad <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Libro Diario</a></li>
-              <li><a href="{{URL::to('contabilidad/librodiario')}}">Libro Diario</a></li>
-              <li><a href="#">ROI</a></li>
-              <li><a href="#">Punto de Equilibrio</a></li>
-              <li><a href="#">Flujo de Caja</a></li>
-              <li class="divider"></li>
-              <li><a href="{{ URL::to('contabilidad/configuracion/') }}">Configuración</a></li>
-            </ul>
-          </li>
-
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">CRM <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Clientes</a></li>
-              <li><a href="#">Empresariales</a></li>
-              <li><a href="#">Proveedores</a></li>
-              <li><a href="#">Campañas</a></li>
-              <li><a href="#">Documentos</a></li>
-              <li><a href="#">Oportunidades de Negocios</a></li>
-              <li class="divider"></li>
-              <li><a href="/CRM">Configuración</a></li>
-            </ul>
-          </li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Compras <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/Compras/SolicitudCotizacion">Solicitudes de Cotización</a></li>
+                  <li><a href="#">Cotizaciones</a></li>
+                  <li><a href="#">**Ordenes de Compras</a></li>
+                  <li><a href="/Compras/OrdenCompra/sinCotizacion/ListaProductos" >Crear Orden Compra sin Cotizacion</a></li>
+                  <li><a href="/Compras/OrdenCompra/conCotizacion/ListaCotizaciones" >Comparar Cotizaciones</a></li>
+                  <li><a href="/Compras/OrdenCompra/Autorizacion/ListaOrdenes" > Autorizar Orden Compra</a></li>
+                  <li><a href="/Compras/OrdenCompra/Autorizacion/ListarOrdenes" > Administrar Orden Compra</a></li>
+                  <li><a href="/Compras/OrdenCompra/GenerarPago/ListaCotizaciones" > generar pago Ordenes de  Compras</a></li>
+                  <li><a href="/Compras/OrdenCompra/Historial/ListarOrdenes" > Historial Ordenes de  Compras</a></li>
+                  <li class="divider"></li>
+                  <li><a href="/Compras">Configuración</a></li>
+                </ul>
+              </li>
 
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Seguridad <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Usuarios</a></li>
-              <li><a href="#">Roles</a></li>
-              <li><a href="#">Logs</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Configuración</a></li>
-            </ul>
-          </li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Inventario <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li>
+                    {{ link_to_route('Inventario.Productos.index', 'Productos')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.Atributos.index', 'Atributos')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.Proveedor.index', 'Proveedores')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.Categoria.index', 'Categorías')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.Ciudad.index', 'Ciudades')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.UnidadMedidas.index', 'Unidades de Medida')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.Horarios.index', 'Horarios')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.FormaPagos.index', 'Formas de Pagos')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.MovimientoInventario.index', 'Movimiento Inventario')}}
+                  </li>
+                  <li>
+                    {{ link_to_route('Inventario.MotivoMovimiento.index', 'Motivo Movimiento Inventario')}}
+                  </li>
+                  <li class="divider"></li>
+                  <li>
+                     <a href="/Inventario">Configuración</a>
+                  </li>
+                </ul>
+              </li>
 
-    </ul>
+              <li class="dropdown">
 
-    <p class="navbar-text navbar-right" style="margin-right: 1em;">Signed in as <a href="#" class="navbar-link">Admin</a></p>
-  </nav>
-  </header>
+                <a href="contabilidad" class="dropdown-toggle" data-toggle="dropdown">Contabilidad <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Libro Diario</a></li>
+                  <li><a href="{{URL::to('contabilidad/librodiario')}}">Libro Diario</a></li>
+                  <li><a href="#">ROI</a></li>
+                  <li><a href="#">Punto de Equilibrio</a></li>
+                  <li><a href="#">Flujo de Caja</a></li>
+                  <li class="divider"></li>
+                  <li><a href="{{ URL::to('contabilidad/configuracion/') }}">Configuración</a></li>
+                </ul>
+              </li>
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">CRM <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Clientes</a></li>
+                  <li><a href="#">Empresariales</a></li>
+                  <li><a href="#">Proveedores</a></li>
+                  <li><a href="#">Campañas</a></li>
+                  <li><a href="#">Documentos</a></li>
+                  <li><a href="#">Oportunidades de Negocios</a></li>
+                  <li class="divider"></li>
+                  <li><a href="/CRM">Configuración</a></li>
+                </ul>
+              </li>
+
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Seguridad <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Usuarios</a></li>
+                  <li><a href="#">Roles</a></li>
+                  <li><a href="#">Logs</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Configuración</a></li>
+                </ul>
+              </li>
+
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+          <p class="navbar-text navbar-right" style="margin-right: 1em;">
+            Signed in as <a href="#" class="navbar-link">Admin</a></p></li>
+        </ul>
+    </div>
+    </div>
+  </div>
+  
   <div class="container col-md-8 col-md-offset-2">
     @yield('main')
   </div>
