9564a85a7ea703ad3e606664a3507ddbef5f3911
Jeremias Velasquez <jerevc@gmail.com>
2014-04-30 19:04:16 -0600

    Validacion

---
+++ pymeSolutionsDev/app/classes/Helpers.php
@@ -0,0 +1,252 @@
+<?php
+class Helpers {
+
+	public static function cualquierProducto(){
+		$query = DB::table('INV_Producto')
+			-> lists('INV_Producto_Codigo',
+					 'INV_Producto_Nombre',
+					 'INV_Producto_Descripcion',
+					 'INV_Producto_Cantidad', 
+					 'INV_Producto_PuntoReorden',
+					 'INV_Producto_PrecioVenta',
+					 'INV_Producto_Activo',
+					 'INV_Producto_PorcentajeDescuento',
+					 'INV_Producto_ID'
+					);
+		$arreglo = $query;
+		return $arreglo;
+	}
+	
+	public static function productoReorden(){
+		$query = DB::table('INV_Producto')
+			-> lists('INV_Producto_Codigo',
+					 'INV_Producto_Nombre',
+					 'INV_Producto_Descripcion',
+					 'INV_Producto_Cantidad', 
+					 'INV_Producto_PuntoReorden',
+					 'INV_Producto_PrecioVenta',
+					 'INV_Producto_Activo',
+					 'INV_Producto_PorcentajeDescuento', 
+					 'INV_Producto_ID'
+					)
+			-> where('INV_Producto_Cantidad', '=', 'INV_Producto_PuntoReorden'
+					);
+		$arreglo = $query;
+		return $arreglo;
+	}
+	
+	public static function proveedorCompras($id){
+		//$query = DB::select('select INV_Proveedor_Nombre, INV_Proveedor_Codigo, INV_Proveedor_Direccion, INV_Proveedor_Email, INV_Proveedor_ID');
+		$query = DB::table('INV_Proveedor')
+			-> lists('INV_Proveedor_Nombre',
+					 'INV_Proveedor_Codigo',
+					 'INV_Proveedor_Direccion',
+					 'INV_Proveedor_Email', 
+					 'INV_Proveedor_ID'
+					);
+		$arreglo = $query;
+		return $arreglo;
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static function InformacionSolicitudesCotizacion(){
+		$query = DB::table('COM_SolicitudCotizacion')
+			-> join('INV_Proveedor', 'Proveedor_idProveedor', '=', 'INV_Proveedor_ID')
+			-> select('COM_SolicitudCotizacion_Codigo as Codigo',
+					  'INV_Proveedor_Nombre as NombreProveedor',
+					  'COM_SolicitudCotizacion_FechaEmision as FechaEmision',
+					  'COM_Usuario_idUsuarioCreo as IdUsuarioCreo',
+					  'COM_SolicitudCotizacion_Recibido as Recibido',
+					  'COM_SolicitudCotizacion_Activo as Activo'
+					)
+			-> orderBy('COM_SolicitudCotizacion_FechaEmision', 'desc')
+			-> orderBy('COM_SolicitudCotizacion_Codigo')
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionSolicitudCotizacion($CodigoSolicitudCotizacion){
+		$query = DB::table('COM_SolicitudCotizacion')
+			-> join('INV_Proveedor', 'Proveedor_idProveedor', '=', 'INV_Proveedor_ID')
+			-> select('COM_SolicitudCotizacion_IdSolicitudCotizacion as IdSolicitudCotizacion',
+					  'COM_SolicitudCotizacion_Codigo as Codigo',
+					  'Proveedor_idProveedor as IdProveedor',
+					  'INV_Proveedor_Nombre as NombreProveedor',
+					  'INV_Proveedor_Direccion as DireccionProveedor',
+					  'INV_Proveedor_Telefono as TelefonoProveedor',
+					  'COM_SolicitudCotizacion_FechaEmision as FechaEmision',
+					  'COM_SolicitudCotizacion_FechaEntrega as FechaEntrega',
+					  'COM_Usuario_idUsuarioCreo as IdUsuarioCreo',
+					  'COM_SolicitudCotizacion_Recibido as Recibido',
+					  'COM_SolicitudCotizacion_Activo as Activo'
+					)
+			-> where('COM_SolicitudCotizacion_Codigo', '=', $CodigoSolicitudCotizacion)
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionProductosSolicitudCotizacion($CodigoSolicitudCotizacion){
+		$query = DB::table('COM_SolicitudCotizacion')
+			-> join('COM_DetalleSolicitudCotizacion', 'COM_SolicitudCotizacion_IdSolicitudCotizacion', '=', 'SolicitudCotizacion_idSolicitudCotizacion')
+			-> join('INV_Producto', 'Producto_idProducto', '=', 'INV_Producto_ID')
+			-> select('INV_Producto_Codigo as Codigo',
+					  'INV_Producto_Nombre as Nombre',
+					  'INV_Producto_Descripcion as Descripcion',
+					  'cantidad as Cantidad'
+					)
+			-> where('COM_SolicitudCotizacion_Codigo', '=', $CodigoSolicitudCotizacion)
+			-> orderBy('cantidad', 'desc')
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionProductoSolicitudCotizacion($CodigoProducto, $CodigoSolicitudCotizacion){
+		$query = DB::table('COM_SolicitudCotizacion')
+			-> join('COM_DetalleSolicitudCotizacion', 'COM_SolicitudCotizacion_IdSolicitudCotizacion', '=', 'SolicitudCotizacion_idSolicitudCotizacion')
+			-> join('INV_Producto', 'Producto_idProducto', '=', 'INV_Producto_ID')
+			-> select('INV_Producto_ID as Id',
+					  'cantidad as Cantidad')
+			-> where('COM_SolicitudCotizacion_Codigo', '=', $CodigoSolicitudCotizacion)
+			-> where('INV_Producto_Codigo', '=', $CodigoProducto)
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionCotizaciones(){
+		$query = DB::table('COM_Cotizacion')
+			-> join('INV_Proveedor', 'COM_Proveedor_idProveedor', '=', 'INV_Proveedor_ID')
+			-> select('COM_Cotizacion_Codigo as Codigo',
+					  'INV_Proveedor_Nombre as NombreProveedor',
+					  'COM_Cotizacion_FechaCreacion as FechaEmision',
+					  'COM_Cotizacion_Vigencia as Vigencia',
+					  'COM_Cotizacion_Activo as Activo'
+					)
+			-> orderBy('COM_Cotizacion_FechaEmision', 'desc')
+			-> orderBy('COM_Cotizacion_Codigo')
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionCotizacion($CodigoCotizacion){
+		$query = DB::table('COM_Cotizacion')
+			-> join('INV_Proveedor', 'COM_Proveedor_idProveedor', '=', 'INV_Proveedor_ID')
+			-> select('INV_Proveedor_Nombre as NombreProveedor',
+					  'INV_Proveedor_Direccion as DireccionProveedor',
+					  'INV_Proveedor_Telefono as TelefonoProveedor',
+					  'COM_Cotizacion_Total as Total',
+					  'COM_Cotizacion_Vigencia as Vigencia'
+					)
+			-> where('COM_Cotizacion_Codigo', '=', $CodigoCotizacion)
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionProductosCotizacion($CodigoCotizacion){
+		$query = DB::table('COM_Cotizacion')
+			-> join('COM_Detalle_Cotizacion', 'COM_Cotizacion_IdCotizacion', '=', 'COM_DetalleCotizacion_IdCotizacion')
+			-> join('INV_Producto', 'COM_Producto_Id_Producto', '=', 'INV_Producto_ID')
+			-> select('INV_Producto_Codigo as Codigo',
+					  'INV_Producto_Nombre as Nombre',
+					  'INV_Producto_Descripcion as Descripcion',
+					  'COM_DetalleCotizacion_Cantidad as Cantidad',
+					  'COM_DetalleCotizacion_PrecioUnitario as Precio'
+					  )
+			-> where('COM_Cotizacion_Codigo', '=', $CodigoCotizacion)
+			-> orderBy('COM_DetalleCotizacion_Cantidad', 'desc')
+			-> get();
+			
+		return $query;
+	}
+	
+	
+	public static function InformacionOrdenesCompra (){
+		$query = DB::table('COM_OrdenCompra')
+			-> join('INV_Proveedor', 'COM_Proveedor_IdProveedor', '=', 'INV_Proveedor_ID')
+			-> select('COM_OrdenCompra_Codigo as Codigo',
+					  'INV_Proveedor_Nombre as NombreProveedor',
+					  'COM_OrdenCompra_FechaEmision as FechaEmision'
+					)
+			-> orderBy('COM_OrdenCompra_FechaEmision', 'desc')
+			-> orderBy('COM_OrdenCompra_Codigo')
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionOrdenCompra ($CodigoOrdenCompra){
+		$query = DB::table('COM_OrdenCompra')
+			-> join('INV_Proveedor', 'COM_Proveedor_IdProveedor', '=', 'INV_Proveedor_ID')
+			-> join('COM_Cotizacion', 'COM_OrdenCompra_IdCotizacion', '=', 'COM_Cotizacion_IdCotizacion')
+			-> select('INV_Proveedor_Nombre as NombreProveedor',
+					  'INV_Proveedor_Email as EmailProveedor',
+					  'INV_Proveedor_Direccion as DireccionProveedor',
+					  'INV_Proveedor_Telefono as TelefonoProveedor',
+					  'COM_Cotizacion_Codigo as CodigoCotizacion',
+					  'COM_Cotizacion_Total as Total',
+					  'COM_OrdenCompra_FechaEmision as FechaEmision',
+					  'COM_OrdenCompra_FechaEntrega as FechaEntrega',
+					  'COM_OrdenCompra_DireccionEntrega as DireccionEntrega',
+					  'COM_OrdenCompra_Activo as Activo'
+					)
+			-> where('COM_OrdenCompra_Codigo', '=', $CodigoOrdenCompra)
+			-> get();
+			
+		return $query;
+	}
+	
+	public static function InformacionProductosOrdenCompra($CodigoOrdenCOmpra){
+		$query = DB::table('COM_OrdenCompra')
+			-> join('COM_DetalleOrdenCompra', 'COM_OrdenCompra_IdOrdenCompra', '=', 'COM_DetalleOrdenCompra_idOrdenCompra')
+			-> join('INV_Producto', 'COM_Producto_idProducto', '=', 'INV_Producto_ID')
+			-> select('INV_Producto_Codigo as Codigo',
+					  'INV_Producto_Nombre as Nombre',
+					  'INV_Producto_Descripcion as Descripcion',
+					  'COM_DetalleOrdenCompra_Cantidad as Cantidad',
+					  'COM_DetalleOrdenCompra_PrecioUnitario as Precio'
+					  )
+			-> where('COM_OrdenCompra_Codigo', '=', $CodigoOrdenCOmpra)
+			-> orderBy('COM_DetalleOrdenCompra_Cantidad', 'desc')
+			-> get();
+			
+		return $query;
+	}
+	
+	
+	public static function CotizacionCapturada($CodigoSolicitudCotizacion){
+		$query = DB::table('COM_Cotizacion')
+			-> join('COM_SolicitudCotizacion', 'COM_Cotizacion_idSolicitudCotizacion', '=', 'COM_SolicitudCotizacion_IdSolicitudCotizacion')
+			-> select('COM_Cotizacion_IdCotizacion')
+			-> where('COM_SolicitudCotizacion_Codigo', '=', $CodigoSolicitudCotizacion)
+			-> get();
+			
+			if (count($query) == 0){
+				return False;
+			}
+			
+			return True;
+	}
+	
+	
+	public static function EstadoActualOrdenCompra($CodigoOrdenCompra){
+		$query = DB::table('COM_OrdenCompra')
+			-> join('COM_TransicionEstado', 'COM_OrdenCompra_IdOrdenCompra', '=', 'COM_TransicionEstado_IdOrdenCompra')
+			-> join('COM_OrdenCompra_TransicionEstado', 'COM_TransicionEstado_OrdenCompra_TransicionEstado_Id', '=', 'COM_OrdenCompra_TransicionEstado_Id')
+			-> join('COM_EstadoOrdenCompra', 'COM_OrdenCompra_TransicionEstado_EstadoActual', '=', 'COM_EstadoOrdenCompra_IdEstadoOrdenCompra')
+			-> select('COM_EstadoOrdenCompra_Nombre as Nombre')
+			-> where('COM_OrdenCompra_Codigo', '=', $CodigoOrdenCompra)
+			-> get();
+			
+			return $query;
+	}
+}
---
+++ pymeSolutionsDev/app/views/COM_OrdenCompra/DetallesOrdenCompra.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.scaffold')
+
+@section('main')
+
+	<?php
+		$CodigoOrdenCompra = Input::get('CodigoOrdenCompra');
+		$OrdenCompra = Helpers::InformacionOrdenCompra($CodigoOrdenCompra);
+		$ProductosOrdenCompra = Helpers::InformacionProductosOrdenCompra($CodigoOrdenCompra);
+	?>
+	
+	
+	<div class="row">
+		<div class="page-header clearfix">
+			<h3 class="pull-left">&nbsp;Cotizaciones &gt; Detalles de la &Oacute;rden de Compra</h3>
+			<div class="pull-right">
+				<a href="/Compras/OrdenesCompra/TodasOrdenesCompra" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Atr&aacute;s</a>
+			</div>
+		</div>
+	</div>
+	
+	{{ Form::open() }}
+		<div class="row">
+			<div class="col-md-4 " ></div>
+			<div class="col-md-5 " style="text-align: center">
+				<h2>Cotizaci&oacute;n</h2>
+				<h5>Empresa X S.A.</h5>
+				<h5>Colonia El &Aacute;lamo, Tegicigalpa, Francisco Moraz&aacute;n</h5>
+				<h5>Honduras, C.A.</h5>
+			</div>
+			<div class="col-md-12 " style="text-align: right">
+				<h5>Tel. 2233-4455, Fax 2244-5566</h5>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-8">
+				<h4>Para:</h4>
+				<h5>DPTO DE VENTAS</h5>
+				<h5>{{ $OrdenCompra[0] -> NombreProveedor }}</h5>
+				<h5>{{ $OrdenCompra[0] -> EmailProveedor }}</h5>
+				<h5>{{ $OrdenCompra[0] -> DireccionProveedor }}</h5>
+				<h5>{{ $OrdenCompra[0] -> TelefonoProveedor }}</h5>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="table-responsive">
+				<table class="table table-striped table-bordered" >
+					<thead>
+						<tr>
+							<th>Codigo</th>
+							<th>Nombre</th>
+							<th>Descripcion</th>
+							<th>Cantidad</th>
+							<th>Precio Unitario</th>
+							<th>Total</th>
+							<th>Unidad</th>
+						</tr>
+					<thead>
+					
+					<tbody>
+						@foreach($ProductosOrdenCompra as $ProductoOrdenCompra)
+							<tr>
+								<td>{{ $ProductoOrdenCompra -> Codigo }}</td>
+								<td>{{ $ProductoOrdenCompra -> Nombre }}</td>
+								<td>{{ $ProductoOrdenCompra -> Descripcion }}</td>
+								<td>{{ $ProductoOrdenCompra -> Cantidad }}</td>
+								<td>Lps. {{ $ProductoOrdenCompra -> Precio }}</td>
+								<td>Lps. {{ $ProductoOrdenCompra -> Cantidad * $ProductoOrdenCompra -> Precio }}</td>
+								<td></td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<label>Fecha de Emisi&oacute;n: </label>
+				{{ $OrdenCompra[0] -> FechaEmision }}
+				
+				<br><br>
+				
+				<label>Fecha de Entrega: </label>
+				{{ $OrdenCompra[0] -> FechaEntrega }}
+				
+				<br><br>
+				
+				<label>Forma de Pago: </label>
+				{{ "Efectivo" }}
+				
+				<br><br>
+				
+				<label>Direcci&oacute;n de Entrega: </label>
+				{{ $OrdenCompra[0] -> DireccionEntrega }}
+				
+				<br><br>
+				
+				<label>Estado: </label>
+				@if ($OrdenCompra[0] -> Activo)
+					{{ "Activa" }}
+				@else
+					{{ "Inactiva" }}
+				@endif
+				
+				
+			</div>
+			
+			<div class="col-md-6" style="text-align: right">
+				<label>Total: </label>
+				{{ $OrdenCompra[0] -> Total }}
+			</div>
+		</div>
+	{{ Form::close() }}
+	
+	
+	
+@stop
