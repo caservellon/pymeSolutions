77275a8607719955a26c5275547ed0cd3f0dffab
andrestorrez <andrews_torres@hotmail.com>
2014-05-19 19:25:43 -0600

    fixed libs with inventario

--- pymeSolutionsDev/app/libs/invContabilidad.php
+++ pymeSolutionsDev/app/libs/invContabilidad.php
@@ -6,10 +6,7 @@
 			return Proveedor::where('INV_Proveedor_ID', $id)->first();
 		}
 
-		public static function getMotivos ()
-		{
-			return MotivoMovimiento::all();
-		}
+
 
 		public static function Activar($id)
 		{
@@ -40,19 +37,6 @@
 		public static function getMotivos ()
 		{
 			return MotivoMovimiento::all();
-		}
-
-		public static function Activar($id)
-		{
-			$motivo = MotivoMovimiento::find($id);
-			if ($motivo) {
-				$motivo->INV_MotivoMovimiento_Activo = 1;
-				if($motivo->update())
-					return 'Concepto Activado';
-				else
-					return 'Error Al Activar Concepto';
-			}
-			return 'Concepto No Encontrado';
 		}
 
 }
