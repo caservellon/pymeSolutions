30f5c2c823e8e3f9e75367916963e78c9275fcb4
Martin Pastor <muko09@unitec.edu>
2014-05-27 17:10:26 -0600

    Imprimir solicitud cotizacion

---
+++ pymeSolutionsDev/app/views/SolicitudCotizacions/imprimir.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.scaffold')
+
+@section('main')
+
+<div class="page-header clearfix">
+      <h3 class="pull-left">Solicitud Cotizacion &gt; <small>Imprimir</small></h3>
+      <div class="pull-right">
+        <a href="{{{ URL::to('Compras/SolicitudCotizacion/Imprimir') }}}" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Atras</a>
+      </div>
+</div>  
+<?php 
+
+ 
+
+$proveedores = invCompras::ProveedorCompras($imprimir->Proveedor_idProveedor); ?>
+            <div class="row">
+    <div class="col-md-4 " ></div>
+    <div class="col-md-4 " style="text-align: center">
+        <h2> Solicitud de Cotizacion</h2>
+        <h5>Empresa X S.A.</h5>
+        <h5>Colonia la America, Tegucigalpa,Francisco Morazán</h5>
+        <h5>Honduras C.A.</h5>
+    </div>
+    <div class="col-md-4 " style="text-align: right">
+        <h5 >Tel.2234-9000 Fax.2234-9000</h5>
+    </div>
+    </div>
+            <div class="row">
+             <div class="col-md-4">
+            <h4>Para:</h4>
+            <h5><?php echo $proveedores->INV_Proveedor_Nombre ?></h5>
+            <h5><?php echo $proveedores->INV_Proveedor_Email ?></h5>
+            <h5><?php echo $proveedores->INV_Proveedor_Direccion ?></h5>
+            <h5><?php echo $proveedores->INV_Proveedor_Telefono ?></h5>
+             </div>
+            </div>
+
+                <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+		<thead>
+			<tr>
+				
+				<th>Nombre Producto</th>
+				<th>Descripcion Producto</th>
+                                <th>Cantidad a Solicitar</th>
+                                <th>Unidad</th>
+                                <th>FormaPago</th>
+                                <?php $lista = CampoLocal::where('GEN_CampoLocal_Activo','1')->where('GEN_CampoLocal_Codigo','LIKE','COM_SC%')->get();
+                                foreach ($lista as $campo){ ?>
+                                    <th><?php echo $campo->GEN_CampoLocal_Nombre ?></th>
+                                <?php } ?>
+                                
+			</tr>
+		</thead>
+
+		<tbody>
+                    
+                    
+                 <?php $detalle = DetalleSolicitudCotizacion::where('SolicitudCotizacion_idSolicitudCotizacion','=', $imprimir->COM_SolicitudCotizacion_IdSolicitudCotizacion)->get(); 
+                    
+                    foreach($detalle as $key){ 
+                       
+                    
+                    $cualquierProducto1= invCompras::ProductoCompras($key->Producto_idProducto); ?>
+                                    <tr>
+					
+                                       
+                                        
+					<td><?php echo $cualquierProducto1->INV_Producto_Nombre; ?></td>
+					<td><?php echo $cualquierProducto1->INV_Producto_Descripcion ?></td>
+                                        <td><?php echo $key->cantidad ?></td>
+					
+                                        
+                                        <?php $unidad= invCompras::UnidadCompras($cualquierProducto1->INV_UnidadMedida_ID) ?>
+                                        <td><?php echo $unidad->INV_UnidadMedida_Nombre ?></td>
+                                        <?php $forma= invCompras::FormaPagoCompras($imprimir->COM_SolicitudCotizacion_FormaPago) ?>
+                                        <td><?php echo $forma->INV_FormaPago_Nombre ?></td>
+                                        <?php foreach (DB::table('GEN_CampoLocal')->where('GEN_CampoLocal_Activo','1')->where('GEN_CampoLocal_Codigo','LIKE','COM_SC%')->get() as $campo){ 
+					    if (DB::table('COM_ValorCampoLocal')->where('COM_CampoLocal_IdCampoLocal',$campo->GEN_CampoLocal_ID)->where('COM_SolicitudCotizacion_IdSolicitudCotizacion',$imprimir->COM_SolicitudCotizacion_IdSolicitudCotizacion)->count() > 0 ){ ?>
+					    	<td><?php echo DB::table('COM_ValorCampoLocal')->where('COM_CampoLocal_IdCampoLocal',$campo->GEN_CampoLocal_ID)->where('COM_SolicitudCotizacion_IdSolicitudCotizacion',$imprimir->COM_SolicitudCotizacion_IdSolicitudCotizacion)->first()->COM_ValorCampoLocal_Valor ?></td>
+                                            <?php }else{ ?>
+					    	<td></td>
+                                        <?php } ?>
+					
+                                       
+                                        
+                                        
+                                </tr>
+                                        <?php }} ?>
+                </tbody> 
+             </table>
+          </div>
+          <div class="col-md-4">
+            <p>Por favor mandar la cantidad de pagos a realizar y el periodo de gracia para realizar el primer pago a partir de enviar la cotizacion</p>
+          </div>
+            <div class="row" >
+                <div class="col-md-6" ><label></label></div>
+                <div class="col-md-6" style="text-align: right"><h5>Nombre del Oficial de Compras</h5></div>
+            </div>
+            <hr>
+@stop
---
+++ pymeSolutionsDev/app/views/SolicitudCotizacions/indexImprimir.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.scaffold')
+
+@section('main')
+
+
+
+<h2 class="sub-header">Todas las Solicitudes de Cotizacion</h2>
+<div class="pull-right">
+        <a href="{{{ URL::to('Compras/SolicitudCotizacion') }}}" class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Regresar</a>
+      </div>
+          <div  class="col-md-9" >
+                          
+                                 <div class="col-xs-5 col-sm-6 col-md-12">
+                                    {{ Form::open(array('route' => 'SolicitudCotizacions.buscarCualquierProveedor')) }}
+                                    {{ Form::label('SearchLabel', 'Busqueda: ', array('class' => 'col-md-2 control-label')) }}
+                                    {{ Form::text('search', null, array('class' => 'col-md-4', 'form-control', 'id' => 'search', 'placeholder'=>'Buscar por nombre, ciudad, codigo..')) }}
+                                    {{ Form::submit('Buscar', array('class' => 'btn btn-success btn-sm' )) }}
+                                    {{ Form::close() }}
+                                </div>
+             
+             
+          </div>
+            @if($SolicitudCotizacions->count())
+            
+                <div class="table-responsive">
+                <table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Codigo</th>
+				<th>Proveedor</th>
+				<th>Fecha</th>
+				<th>Usuario</th>
+				<th>Estado</th>
+                <th>FormaPago</th>
+				@foreach($CamposLocales as $CampoLocal)
+                                                @if($CampoLocal->GEN_CampoLocal_Activo==1)
+						<th>{{{ $CampoLocal->GEN_CampoLocal_Nombre }}}</th>
+                                                @endif
+				@endforeach
+				<th>Activo</th>
+				
+			</tr>
+		</thead>
+
+		<tbody>
+			@foreach ($SolicitudCotizacions as $editars)
+			@if($editars->COM_SolicitudCotizacion_Imprimir == 0)
+				<tr>
+					<td>{{{ $editars->COM_SolicitudCotizacion_Codigo }}}</td>
+                                        <?php $proveedor= invCompras::ProveedorCompras($editars->Proveedor_idProveedor) ?>
+					<td><a>{{{ $proveedor->INV_Proveedor_Nombre }}}</a></td>
+					<td>{{{ $editars->COM_SolicitudCotizacion_FechaEmision }}}</td>
+					@if($editars->COM_Usuario_idUsuarioCreo==1)
+                                           <td>Juan</td>
+                                           @endif
+					@if($editars->COM_SolicitudCotizacion_Recibido == 1)
+							<td>Recibido</td>
+						@else
+							<td>En Espera</td>
+						@endif
+                                         <?php $forma= invCompras::FormaPagoCompras($editars->COM_SolicitudCotizacion_FormaPago) ?>
+                                        <td>{{{ $forma->INV_FormaPago_Nombre }}}</td>
+                                        
+					@foreach (DB::table('GEN_CampoLocal')->where('GEN_CampoLocal_Activo','1')->where('GEN_CampoLocal_Codigo','LIKE','COM_SC%')->get() as $campo)
+					    @if (DB::table('COM_ValorCampoLocal')->where('COM_CampoLocal_IdCampoLocal',$campo->GEN_CampoLocal_ID)->where('COM_SolicitudCotizacion_IdSolicitudCotizacion',$editars->COM_SolicitudCotizacion_IdSolicitudCotizacion)->count() > 0 )
+					    	<td>{{{ DB::table('COM_ValorCampoLocal')->where('COM_CampoLocal_IdCampoLocal',$campo->GEN_CampoLocal_ID)->where('COM_SolicitudCotizacion_IdSolicitudCotizacion',$editars->COM_SolicitudCotizacion_IdSolicitudCotizacion)->first()->COM_ValorCampoLocal_Valor }}}</td>
+					    @else
+					    	<td></td>
+					    @endif
+					@endforeach
+					
+					@if($editars->COM_SolicitudCotizacion_Activo == 1)
+							<td>Activo</td>
+						@else
+							<td>Inactivo</td>
+						@endif
+                                        
+					
+                   <td>{{ link_to_route('impresion', 'Impresion', array('solCot'=>$editars->COM_SolicitudCotizacion_IdSolicitudCotizacion), array('class' => 'btn btn-success')) }}</td>
+
+
+        
+                                </tr>
+								
+                 @endif    
+                @endforeach
+                
+            
+                </tbody> 
+             </table>
+          </div>
+   
+                
+
+
+
+
+
+
+
+            
+
+
+@endif
+<div>
+                <h6>{{$SolicitudCotizacions->links()}}</h6>
+            </div>
+
+@stop
