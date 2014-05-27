22e02d3268cd877a689011320c9229e5848c30a3
Jorge Caballero <jorge@2build.it>
2014-05-26 19:18:26 -0600

    Arregla dropdown asignacion de role edit

--- pymeSolutionsDev/app/views/Usuarios/edit.blade.php
+++ pymeSolutionsDev/app/views/Usuarios/edit.blade.php
@@ -48,7 +48,7 @@
 		<div class="form-group">
 			{{ Form::label('SEG_Roles_SEG_Roles_ID', 'Role: *', array('class' => 'col-md-2 control-label')) }}
 			<div class="col-md-5">
-        	{{ Form::select('SEG_Roles_SEG_Roles_ID', $roles, '1', array('class' => 'value-input form-control', 'id' => 'SEG_Roles_SEG_Roles_ID')) }}
+        	{{ Form::select('SEG_Roles_SEG_Roles_ID', $roles, $User->SEG_Roles_SEG_Roles_ID, array('class' => 'value-input form-control', 'id' => 'SEG_Roles_SEG_Roles_ID')) }}
         	</div>
 		</div> 
 
