c86a3c1389d370518401a4bfd6cc842075f550af
Jeremias Velasquez <jerevc@gmail.com>
2014-05-28 18:50:19 -0600

    Cambios minimos

--- pymeSolutionsDev/app/views/COM_Cotizacion/CapturarCotizacionCapturar.blade.php
+++ pymeSolutionsDev/app/views/COM_Cotizacion/CapturarCotizacionCapturar.blade.php
@@ -208,19 +208,19 @@
 					
 					@if($HayError)
 						<div class="form-group has-error has-feedback">
-							<label class="control-label" for="inputError2">Impuesto</label>
+							<label class="control-label" for="inputError2">Impuesto(%)</label>
 							{{ Form::text('ImpuestoCotizacion', null, array('class' => 'form-control', 'onChange' => 'AsignarTotales("ImpuestoCotizacion", 0)')) }}
 							<span class="glyphicon glyphicon-remove form-control-feedback"></span>
 						</div>
 					@else
 						<div class="form-group has-success has-feedback">
-							<label class="control-label" for="inputSuccess2">Impuesto</label>
+							<label class="control-label" for="inputSuccess2">Impuesto(%)</label>
 							{{ Form::text('ImpuestoCotizacion', null, array('class' => 'form-control', 'onChange' => 'AsignarTotales("ImpuestoCotizacion", 0)')) }}
 							<span class="glyphicon glyphicon-ok form-control-feedback"></span>
 						</div>
 					@endif
 				@else
-					{{ Form::label('Impuesto', 'Impuesto') }}
+					{{ Form::label('Impuesto', 'Impuesto(%)') }}
 					{{ Form::text('ImpuestoCotizacion', null, array('class' => 'form-control', 'onChange' => 'AsignarTotales("ImpuestoCotizacion", 0)')) }}
 				@endif
 				
