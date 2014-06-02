1a8c2199e5eddaa5bd5ea32864490f13a53d243d
andrestorrez <andrews_torres@hotmail.com>
2014-06-02 17:43:31 -0600

    Devluciones de compras

---
+++ pymeSolutionsDev/app/models/CON_Proveedor.php
@@ -0,0 +1,8 @@
+<?php
+
+class CON_Proveedor extends Eloquent {
+	//protected $guarded = array();
+	protected $table = 'CON_Proveedores';
+	protected $primaryKey = 'INV_Proveedores_ID';
+	public $timestamps=false;
+}
