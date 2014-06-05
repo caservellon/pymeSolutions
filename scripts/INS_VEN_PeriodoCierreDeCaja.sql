3eaf76fe91de117c6c1c4d47cd84202e17f20ee2
Jorge Caballero <jorge@2build.it>
2014-06-04 23:08:05 -0600

    Agrega AI a tablas de Seguridad

--- scripts/INS_VEN_PeriodoCierreDeCaja.sql
+++ scripts/INS_VEN_PeriodoCierreDeCaja.sql
@@ -2,6 +2,5 @@
 -- Query: 
 -- Date: 2014-02-17 08:44
 */
-INSERT INTO `VEN_PeriodoCierreDeCaja` (`VEN_PeriodoCierreDeCaja_id`,`VEN_PeriodoCierreDeCaja_Codigo`,`VEN_PeriodoCierreDeCaja_ValorHoras`,`VEN_PeriodoCierreDeCaja_Estado`,`VEN_PeriodoCierreDeCaja_HoraPartida`) VALUES (1,'CC1','36','1','7:00');
-INSERT INTO `VEN_PeriodoCierreDeCaja` (`VEN_PeriodoCierreDeCaja_id`,`VEN_PeriodoCierreDeCaja_Codigo`,`VEN_PeriodoCierreDeCaja_ValorHoras`,`VEN_PeriodoCierreDeCaja_Estado`,`VEN_PeriodoCierreDeCaja_HoraPartida`) VALUES (2,'CC2','48','1','20:30');
-INSERT INTO `VEN_PeriodoCierreDeCaja` (`VEN_PeriodoCierreDeCaja_id`,`VEN_PeriodoCierreDeCaja_Codigo`,`VEN_PeriodoCierreDeCaja_ValorHoras`,`VEN_PeriodoCierreDeCaja_Estado`,`VEN_PeriodoCierreDeCaja_HoraPartida`) VALUES (3,'CC3','96','0','8:00');
+INSERT INTO `VEN_PeriodoCierreDeCaja` (`VEN_PeriodoCierreDeCaja_id`,`VEN_PeriodoCierreDeCaja_Codigo`,`VEN_PeriodoCierreDeCaja_ValorHoras`,`VEN_PeriodoCierreDeCaja_Estado`,`VEN_PeriodoCierreDeCaja_HoraPartida`) VALUES (1,'CC1','9','1','8:00');
+INSERT INTO `VEN_Caja` (`VEN_Caja_id`,`VEN_Caja_Codigo`,`VEN_Caja_Numero`,`VEN_Caja_Estado`,`VEN_Caja_SaldoInicial`,`VEN_Caja_TimeStamp`,`VEN_PeriodoCierreDeCaja_VEN_PeriodoCierreDeCaja_id`) VALUES (1,'CAJ-0001',1,1,0,NULL,1);
