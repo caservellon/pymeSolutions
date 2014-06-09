aefaf25695a8022c06e430690dfc114d34440cbf
erazo14 <erazo.14@hotmail.com>
2014-06-09 11:07:37 -0600

    Seguridad en Contabilidad parte 1

--- pymeSolutionsDev/app/controllers/CierrePeriodoController.php
+++ pymeSolutionsDev/app/controllers/CierrePeriodoController.php
@@ -10,148 +10,177 @@ class CierrePeriodoController extends BaseController {
 
 	public function index()
 	{
-		if(Request::isMethod('post')){
-			if (Cache::has('PeriodosContables')){
-				$periodos=Cache::get('PeriodosContables');
-				$periodo=$periodos[Input::get('periodo')][1];
-				Cache::put('PeriodoContable',$periodo,30); 
-				Cache::forget('PeriodosContables');	
-				return View::Make('CierrePeriodo.index');
-			}
-			return Redirect::route('con.cierreperiodo');
-			
-		}else{
-			$array=array();
-			$CP=ClasificacionPeriodo::orderBy('CON_ClasificacionPeriodo_CatidadDias','Desc')->get();
-			$counter=0;
-			$actual_date=date('Y-m-d H:m:s');
-			for ($i=0; $i < sizeof($CP); $i++) { 
-				$periodo=$CP[$i]->PeriodosContables()
-                    ->orderBy('CON_PeriodoContable_FechaFinal','Desc')
-                    ->take(1)->get();
-                $periodo=$periodo[0];
-				$final_date=$periodo->CON_PeriodoContable_FechaFinal;
-				if ($final_date>$actual_date){
-					//continue;
-				}
-				$array[$counter][0]=$CP[$i];
-				$array[$counter][1]=$periodo;
-				$counter++;
-			}
-			Cache::put('PeriodosContables', $array, 10);
-			return View::Make('CierrePeriodo.form',compact('array'));
-		}
-        
+        if (Seguridad::GenerarCierrePeriodo()) {
+    		if(Request::isMethod('post')){
+    			if (Cache::has('PeriodosContables')){
+    				$periodos=Cache::get('PeriodosContables');
+    				$periodo=$periodos[Input::get('periodo')][1];
+    				Cache::put('PeriodoContable',$periodo,30); 
+    				Cache::forget('PeriodosContables');	
+    				return View::Make('CierrePeriodo.index');
+    			}
+    			return Redirect::route('con.cierreperiodo');
+    			
+    		}else{
+    			$array=array();
+    			$CP=ClasificacionPeriodo::orderBy('CON_ClasificacionPeriodo_CatidadDias','Desc')->get();
+    			$counter=0;
+    			$actual_date=date('Y-m-d H:m:s');
+    			for ($i=0; $i < sizeof($CP); $i++) { 
+    				$periodo=$CP[$i]->PeriodosContables()
+                        ->orderBy('CON_PeriodoContable_FechaFinal','Desc')
+                        ->take(1)->get();
+                    $periodo=$periodo[0];
+    				$final_date=$periodo->CON_PeriodoContable_FechaFinal;
+    				if ($final_date>$actual_date){
+    					//continue;
+    				}
+    				$array[$counter][0]=$CP[$i];
+    				$array[$counter][1]=$periodo;
+    				$counter++;
+    			}
+    			Cache::put('PeriodosContables', $array, 10);
+    			return View::Make('CierrePeriodo.form',compact('array'));
+    		}
+        }else{
+            return Redirect::to('/403');
+        }
     }
 
     public function run(){
-        $array=array();
-        $current=0;
-        try {
+        if (Seguridad::GenerarCierrePeriodo()) {
+            $array=array();
+            $current=0;
+            try {
 
-            $periodo=Cache::get('PeriodoContable');
-            $periodoId=$periodo->CON_PeriodoContable_ID;
-            
-            DB::beginTransaction();
+                $periodo=Cache::get('PeriodoContable');
+                $periodoId=$periodo->CON_PeriodoContable_ID;
+                
+                DB::beginTransaction();
 
-            $result=DB::statement(DB::raw('CALL CON_Mayorizacion(?,?);'),
-                array($periodoId,$periodo->CON_PeriodoContable_FechaFinal));
-            
-            $array[0]=array(0,'30%');
+                $result=DB::statement(DB::raw('CALL CON_Mayorizacion(?,?);'),
+                    array($periodoId,$periodo->CON_PeriodoContable_FechaFinal));
+                
+                $array[0]=array(0,'30%');
 
-            //Simula Creacion de Balanza
-            $array[1]=array(1,'40%');
-            Cache::forever('EstadoCierre',$array);
-            $current+=2;
-            //sleep(2);
-            //Estado
-            $result=DB::statement(DB::raw('CALL CON_EstadoResultados(?)'),
-                array($periodoId));
-            $array=array_merge(Cache::get('EstadoCierre'),array(array(2,'65%')));
-            Cache::forever('EstadoCierre',$array);
-            $current++;
-            //sleep(2);
-            //Balance
-            $result=DB::statement(DB::raw('CALL CON_BalanceGeneral(?)'),
-                array($periodoId));
-            $array=array_merge(Cache::get('EstadoCierre'),array(array(3,'85%')));
-            Cache::forever('EstadoCierre',$array);
-            $current++;
-            //sleep(2);
-            //Nuevo Periodo
-            $result=DB::statement(DB::raw('CALL CON_NuevoPeriodo(?)'),
-                array($periodoId));
-            $array=array_merge(Cache::get('EstadoCierre'),array(array(4,'100%')));
+                //Simula Creacion de Balanza
+                $array[1]=array(1,'40%');
+                Cache::forever('EstadoCierre',$array);
+                $current+=2;
+                //sleep(2);
+                //Estado
+                $result=DB::statement(DB::raw('CALL CON_EstadoResultados(?)'),
+                    array($periodoId));
+                $array=array_merge(Cache::get('EstadoCierre'),array(array(2,'65%')));
+                Cache::forever('EstadoCierre',$array);
+                $current++;
+                //sleep(2);
+                //Balance
+                $result=DB::statement(DB::raw('CALL CON_BalanceGeneral(?)'),
+                    array($periodoId));
+                $array=array_merge(Cache::get('EstadoCierre'),array(array(3,'85%')));
+                Cache::forever('EstadoCierre',$array);
+                $current++;
+                //sleep(2);
+                //Nuevo Periodo
+                $result=DB::statement(DB::raw('CALL CON_NuevoPeriodo(?)'),
+                    array($periodoId));
+                $array=array_merge(Cache::get('EstadoCierre'),array(array(4,'100%')));
 
-            DB::commit();
-            Cache::forget('PeriodoContable');
-            Cache::forever('EstadoCierre',$array);
-            
-        } catch (Exception $e) {
-           // Cache::forever('EstadoCierre',array('Error'=>));
-            DB::rollBack();
+                DB::commit();
+                Cache::forget('PeriodoContable');
+                Cache::forever('EstadoCierre',$array);
+                
+            } catch (Exception $e) {
+               // Cache::forever('EstadoCierre',array('Error'=>));
+                DB::rollBack();
+                return Response::json(
+                    array('success'=>false,
+                          'error'=>json_encode($e->getMessage()),
+                          'current'=> $current
+                    ));
+            }
             return Response::json(
-                array('success'=>false,
-                      'error'=>json_encode($e->getMessage()),
-                      'current'=> $current
-                ));
+                array('success'=>true,
+                        'msg'=>View::make('CierrePeriodo._success')->render() ));
+        }else{
+                return Redirect::to('/403');
         }
-        return Response::json(
-            array('success'=>true,
-                    'msg'=>View::make('CierrePeriodo._success')->render() ));
     }
 
     public function retrieve(){
-        $array=Cache::get('EstadoCierre');
-        Cache::forever('EstadoCierre',array());
-        return Response::json($array);
+        if (Seguridad::GenerarCierrePeriodo()) {
+            $array=Cache::get('EstadoCierre');
+            Cache::forever('EstadoCierre',array());
+            return Response::json($array);
+        }else{
+                return Redirect::to('/403');
+        }
     }
 
     public function mayorizar(){
-    	if (Request::ajax()){
-              $periodo=Cache::get('PeriodoContable');
-    		  $result=DB::select('CALL CON_Mayorizacion(?,?);',
-    			 array($periodo->CON_PeriodoContable_ID,$periodo->CON_PeriodoContable_FechaFinal));
-           
-    		return json_encode($result);
-    	}
-    	return "error";
+        if (Seguridad::GenerarCierrePeriodo()) {
+        	if (Request::ajax()){
+                  $periodo=Cache::get('PeriodoContable');
+        		  $result=DB::select('CALL CON_Mayorizacion(?,?);',
+        			 array($periodo->CON_PeriodoContable_ID,$periodo->CON_PeriodoContable_FechaFinal));
+               
+        		return json_encode($result);
+        	}
+        	return "error";
+        }else{
+                return Redirect::to('/403');
+        }
     }
 
     public function balanza(){
-
-    	if(Request::ajax()){
-    		return ':D';
-    	}
-    	return 'error';
+        if (Seguridad::GenerarCierrePeriodo()) {
+        	if(Request::ajax()){
+        		return ':D';
+        	}
+        	return 'error';
+        }else{
+                return Redirect::to('/403');
+        }
     }
 
     public function estado(){
-    	if(Request::ajax()){
-    		$result=DB::statement(DB::raw('CALL CON_EstadoResultados(?)'),
-    			array(Cache::get('PeriodoContable')->CON_PeriodoContable_ID));
-    		return json_encode($result);
-    	}
-    	return 'error';
-
+        if (Seguridad::GenerarCierrePeriodo()) {
+            if(Request::ajax()){
+        		$result=DB::statement(DB::raw('CALL CON_EstadoResultados(?)'),
+        			array(Cache::get('PeriodoContable')->CON_PeriodoContable_ID));
+        		return json_encode($result);
+        	}
+        	return 'error';
+        }else{
+                return Redirect::to('/403');
+        }
     }
 
     public function balance(){
-    	if(Request::ajax()){
-    		$result=DB::statement(DB::raw('CALL CON_BalanceGeneral(?)'),
-    			array(Cache::get('PeriodoContable')->CON_PeriodoContable_ID));
-    		return json_encode($result);
-    	}
-    	return 'error';
+        if (Seguridad::GenerarCierrePeriodo()) {
+        	if(Request::ajax()){
+        		$result=DB::statement(DB::raw('CALL CON_BalanceGeneral(?)'),
+        			array(Cache::get('PeriodoContable')->CON_PeriodoContable_ID));
+        		return json_encode($result);
+        	}
+        	return 'error';
+        }else{
+                return Redirect::to('/403');
+        }
     }
 
     public function nuevoPeriodo(){
-    	if(Request::ajax()){
-    		$periodo=Cache::get('PeriodoContable');
-    		$result=DB::statement(DB::raw('CALL CON_NuevoPeriodo(?)'),
-    			array($periodo->CON_PeriodoContable_ID));
-    		return json_encode($result);
-    	}
+    	if (Seguridad::GenerarCierrePeriodo()) {
+            if(Request::ajax()){
+        		$periodo=Cache::get('PeriodoContable');
+        		$result=DB::statement(DB::raw('CALL CON_NuevoPeriodo(?)'),
+        			array($periodo->CON_PeriodoContable_ID));
+        		return json_encode($result);
+        	}
+        }else{
+                return Redirect::to('/403');
+        }
     }
 	
 
--- pymeSolutionsDev/app/controllers/LibroDiarioController.php
+++ pymeSolutionsDev/app/controllers/LibroDiarioController.php
@@ -4,67 +4,75 @@ class LibroDiarioController extends BaseController {
 	
 	public function index()
 	{
-		$nPag=3;
-		if(Request::Ajax()){
-			$LibroDiario = LibroDiario::where('CON_LibroDiario_FechaCreacion','>=',Input::get('date1'))
-				->where('CON_LibroDiario_FechaCreacion','<=',Input::get('date2'))
-				->paginate($nPag);
-			$Asientos=$this->getAsientos($LibroDiario);
-			return View::make('LibroDiario.table')
-				->with('LibroDiario',$Asientos)
-				->with('Pages',$LibroDiario);
+		if (Seguridad::VerLibroDiario()) {
+			$nPag=3;
+			if(Request::Ajax()){
+				$LibroDiario = LibroDiario::where('CON_LibroDiario_FechaCreacion','>=',Input::get('date1'))
+					->where('CON_LibroDiario_FechaCreacion','<=',Input::get('date2'))
+					->paginate($nPag);
+				$Asientos=$this->getAsientos($LibroDiario);
+				return View::make('LibroDiario.table')
+					->with('LibroDiario',$Asientos)
+					->with('Pages',$LibroDiario);
+			}else{
+				$LibroDiario = LibroDiario::paginate($nPag);
+				$Asientos=$this->getAsientos($LibroDiario);
+				$PeriodoContable = PeriodoContable::orderBy('CON_PeriodoContable_FechaInicio')->first();
+				return View::make('LibroDiario.index')
+					->with('PeriodoContable',$PeriodoContable)
+					->with('LibroDiario',$Asientos)
+					->with('Pages',$LibroDiario);
+			}
 		}else{
-			$LibroDiario = LibroDiario::paginate($nPag);
-			$Asientos=$this->getAsientos($LibroDiario);
-			$PeriodoContable = PeriodoContable::orderBy('CON_PeriodoContable_FechaInicio')->first();
-			return View::make('LibroDiario.index')
-				->with('PeriodoContable',$PeriodoContable)
-				->with('LibroDiario',$Asientos)
-				->with('Pages',$LibroDiario);
+			return Redirect::to('/403');
 		}
 	}
 
 	protected function getAsientos($LibroDiario){
-		$Asientos=array();
-		foreach ($LibroDiario as $Asiento) {
-		//	$Cuentas=CuentaMotivo::where('CON_MotivoTransaccion_ID','=',$Asiento->CON_MotivoTransaccion_ID)->get();
-			
-			$CuentaMotivo= CuentaMotivo::where('CON_MotivoTransaccion_ID','=',$Asiento->CON_MotivoTransaccion_ID)->get();
+		if (Seguridad::VerLibroDiario()) {
+			$Asientos=array();
+			foreach ($LibroDiario as $Asiento) {
+			//	$Cuentas=CuentaMotivo::where('CON_MotivoTransaccion_ID','=',$Asiento->CON_MotivoTransaccion_ID)->get();
+				
+				$CuentaMotivo= CuentaMotivo::where('CON_MotivoTransaccion_ID','=',$Asiento->CON_MotivoTransaccion_ID)->get();
 
-			if ($CuentaMotivo[0]->CON_CuentaMotivo_DebeHaber!=1
-				XOR $Asiento->CON_LibroDiario_AsientoReversion==1
-				){
+				if ($CuentaMotivo[0]->CON_CuentaMotivo_DebeHaber!=1
+					XOR $Asiento->CON_LibroDiario_AsientoReversion==1
+					){
 
-			   $Debe= CatalogoContable::find($CuentaMotivo[0]->CON_CatalogoContable_ID);
-			   $Haber = CatalogoContable::find($CuentaMotivo[1]->CON_CatalogoContable_ID);
+				   $Debe= CatalogoContable::find($CuentaMotivo[0]->CON_CatalogoContable_ID);
+				   $Haber = CatalogoContable::find($CuentaMotivo[1]->CON_CatalogoContable_ID);
+				}
+				else{
+					$Haber = CatalogoContable::find($CuentaMotivo[0]->CON_CatalogoContable_ID);
+					$Debe = CatalogoContable::find($CuentaMotivo[1]->CON_CatalogoContable_ID);
+				}
+
+				$Debe=$Debe->CON_CatalogoContable_Nombre;
+				$Haber=$Haber->CON_CatalogoContable_Nombre; 
+
+				$Asientos[$Asiento->CON_LibroDiario_ID][0]=array(
+					'no'=> $Asiento->CON_LibroDiario_ID,
+					'fecha'=> $Asiento->CON_LibroDiario_FechaCreacion,
+					'cuenta'=>$Debe,
+					'observacion'=>$Asiento->CON_LibroDiario_Observacion,
+					'monto'=>$Asiento->CON_LibroDiario_Monto,
+					'reversion'=>$Asiento->CON_LibroDiario_AsientoReversion,
+					'revertido'=>$Asiento->CON_LibroDiario_Revertido);
+				$Asientos[$Asiento->CON_LibroDiario_ID][1]=array(
+					'cuenta'=>"\t".$Haber,
+					'monto'=> $Asiento->CON_LibroDiario_Monto);
 			}
-			else{
-				$Haber = CatalogoContable::find($CuentaMotivo[0]->CON_CatalogoContable_ID);
-				$Debe = CatalogoContable::find($CuentaMotivo[1]->CON_CatalogoContable_ID);
-			}
-
-			$Debe=$Debe->CON_CatalogoContable_Nombre;
-			$Haber=$Haber->CON_CatalogoContable_Nombre; 
-
-			$Asientos[$Asiento->CON_LibroDiario_ID][0]=array(
-				'no'=> $Asiento->CON_LibroDiario_ID,
-				'fecha'=> $Asiento->CON_LibroDiario_FechaCreacion,
-				'cuenta'=>$Debe,
-				'observacion'=>$Asiento->CON_LibroDiario_Observacion,
-				'monto'=>$Asiento->CON_LibroDiario_Monto,
-				'reversion'=>$Asiento->CON_LibroDiario_AsientoReversion,
-				'revertido'=>$Asiento->CON_LibroDiario_Revertido);
-			$Asientos[$Asiento->CON_LibroDiario_ID][1]=array(
-				'cuenta'=>"\t".$Haber,
-				'monto'=> $Asiento->CON_LibroDiario_Monto);
+			return $Asientos;
+		}else{
+			return Redirect::to('/403');
 		}
-		return $Asientos;
 	}
 
 	Public function reversion(){
 	 	$id = Input::get('id');
 	 	$diario = LibroDiario::find($id);
-	 	if((!is_null($diario) && $diario->CON_LibroDiario_AsientoReversion == 0) && ($diario->CON_LibroDiario_Revertido == 0)){
+	 	if(Seguridad::RevertirAsientosContables() &&(!is_null($diario) && $diario->CON_LibroDiario_AsientoReversion == 0) && ($diario->CON_LibroDiario_Revertido == 0)){
 			$libro = new LibroDiario;
 			$libro->CON_LibroDiario_Monto = $diario->CON_LibroDiario_Monto;
 			$libro->CON_MotivoTransaccion_ID = $diario->CON_MotivoTransaccion_ID;
